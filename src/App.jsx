0b1b7977ccd79230dcede93e88c43c34afadc809
Nava8 <sebastian.navarro@tecsyo.edu.pe>
2023-06-19 11:54:43 -0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Index from './pages/Index';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import Register from './pages/Register';
+import Parentesco from './pages/apoderados/Parentesco';
+import Vehiculos from './pages/vehiculos/Vehiculos';
+import VehiculosDocumentos from './pages/vehiculos/Vehiculos_documentos'; 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/parentesco" element={<Parentesco />} />
+        <Route path="/vehiculos" element={<Vehiculos />} />
+        <Route path="/vehiculos-documentos" element={<VehiculosDocumentos />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
